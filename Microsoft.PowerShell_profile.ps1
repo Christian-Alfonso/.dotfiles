adf4db3c19de8cc8eb76a4fd6615bbeefc89afb8
Christian Alfonso <christian.a.alfonso@outlook.com>
2025-06-25 19:50:57 -0700

    WIP setup scripts for the configurations in this repository

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,10 @@
+# Environment variables
+$env:PSREADLINE_VTINPUT=1
+
+Set-PSReadLineKeyHandler -Chord 'Ctrl+Backspace' -Function BackwardKillWord
+# Get-PSReadLineKeyHandler -Chord Ctrl+Backspace, Ctrl+w
+
+# Oh-My-Posh wants this
+[Console]::OutputEncoding = [Text.Encoding]::UTF8
+
 oh-my-posh init pwsh --config "$env:USERPROFILE\theme.omp.json" | Invoke-Expression
