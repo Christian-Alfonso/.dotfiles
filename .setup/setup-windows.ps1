723109c18c13a43e6721cb64cbf96fc05ae80edd
Christian Alfonso <christian.a.alfonso@outlook.com>
2025-08-02 01:58:10 -0700

    Fix issues with setup scripts, line encoding to LF for files used by WSL

--- .setup/setup-windows.ps1
+++ .setup/setup-windows.ps1
@@ -14,7 +14,7 @@
 #   (or)
 #   winget install --id Microsoft.PowerShell.Preview --source winget
 #
-#   wsl --install
+#   wsl --install Ubuntu
 #
 # Then restart your machine for WSL to finish installing.
 #
@@ -29,10 +29,12 @@ param(
     [switch] $NormalGit
 )
 
+$ErrorActionPreference = "Stop"
+
 Write-Host "Installing applications, this may take a while..."
 
 # Git needs to be installed before anything, since other programs rely on it
-if (-Not $NormalGit) {
+if (!$NormalGit) {
     winget install --id Microsoft.Git --source winget
 }
 else {
@@ -44,15 +46,19 @@ else {
 #
 
 # Install Oh-My-Posh for PowerShell theme and also Nerd Font installation
-winget install --id JanDeDobbeleer.OhMyPosh --source winget
+& winget install --id JanDeDobbeleer.OhMyPosh --source winget
+
+$OhMyPoshConfig = Convert-Path "$PSScriptRoot\..\theme-v2.omp.json"
+Copy-Item -Path $OhMyPoshConfig -Destination "$env:USERPROFILE\theme-v2.omp.json" 
 
 # Install the DroidSansM Nerd Font families
-$OhMyPosh = $env:LOCALAPPDATA\Programs\oh-my-posh\bin\oh-my-posh.exe
-$OhMyPosh font install droidsansmono
+$OhMyPosh = "$env:LOCALAPPDATA\Programs\oh-my-posh\bin\oh-my-posh.exe"
+& $OhMyPosh font install droidsansmono
 
 # Copy over the PowerShell 7 profile to $PROFILE so that it runs
 # every time that the prompt opens
-Start-Process powershell -Verb RunAs -ArgumentList "-NoExit -Command cd '$PWD'; Copy-Item -Path .\Microsoft.PowerShell_profile.ps1 -Destination $PROFILE; exit"
+$ProfilePath = Convert-Path "$PSScriptRoot\..\Microsoft.PowerShell_profile.ps1"
+Copy-Item -Path $ProfilePath -Destination $PROFILE
 
 # Install editors
 winget install --id Microsoft.VisualStudioCode --source winget
@@ -61,25 +67,31 @@ winget install --id Neovim.Neovim --source winget
 Write-Host "Copying configurations over for each application..."
 
 # Replace configuration for WT
-Copy-Item -Path $PSScriptRoot\.wt\settings.json -Destination "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+Copy-Item -Path "$PSScriptRoot\..\.wt\settings.json" -Destination "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 
 # Replace configurations for VSCode
-Copy-Item -Path $PSScriptRoot\.vscode\settings.json -Destination "$env:APPDATA\Code\User\settings.json"
-Copy-Item -Path $PSScriptRoot\.vscode\keybindings.json -Destination "$env:APPDATA\Code\User\keybindings.json"
+Copy-Item -Path "$PSScriptRoot\..\.vscode\settings.json" -Destination "$env:APPDATA\Code\User\settings.json"
+Copy-Item -Path "$PSScriptRoot\..\.vscode\keybindings.json" -Destination "$env:APPDATA\Code\User\keybindings.json"
 
 # Copy over Neovim configuration to %LocalAppData%\nvim using its own script
-$PSScriptRoot\.nvim\CopyConfiguration.ps1
+$NeovimCopyConfig = Convert-Path "$PSScriptRoot\..\.nvim\CopyConfiguration.ps1"
+& $NeovimCopyConfig
 
 #
 # Configure WSL
 #
 
-wsl.exe -e ./setup-wsl.sh
+$SetupWSLScriptPath = wsl.exe wslpath -a -u "$PSScriptRoot\setup-wsl.sh".Replace("\", "\\")
+wsl.exe -e bash -c "chmod +x $SetupWSLScriptPath; $SetupWSLScriptPath"
 
 #
 # Manual Configurations
 #
 
-Write-Host "The following need to be manually configured:"
-Write-Host "- PowerToys"
+Write-Host @"
 
+The following applications require manual configuration that cannot be done from scripts:
+    - PowerToys: Use the `"backup-powertoys.ps1`" script with the `"-Restore`" parameter
+    to package and copy the config files, and then use the `"Restore`" button in PowerToys
+    to force it to restore the configuration.
+"@
--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,10 @@
 # Environment variables
 $env:PSREADLINE_VTINPUT=1
 
+# This should already be bound by default, but just in case
 Set-PSReadLineKeyHandler -Chord 'Ctrl+Backspace' -Function BackwardKillWord
-# Get-PSReadLineKeyHandler -Chord Ctrl+Backspace, Ctrl+w
 
 # Oh-My-Posh wants this
 [Console]::OutputEncoding = [Text.Encoding]::UTF8
 
-oh-my-posh init pwsh --config "$env:USERPROFILE\theme.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:USERPROFILE\theme-v2.omp.json" | Invoke-Expression
